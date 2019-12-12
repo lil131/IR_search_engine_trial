9036757cacb0ebe28b80dc61065af8d5f460b881
LLL <lil131@pitt.edu>
2019-12-11 19:56:25 -0500

    modified single page font size

--- IR_finalproject/uptown/js/single_page.js
+++ IR_finalproject/uptown/js/single_page.js
@@ -30,25 +30,25 @@ fetch("http://0.0.0.0:5000/searchone/" + para)
         let h2 = createNode('h2');
         h2.innerHTML = movie.name;
         append(movieInfo, h2);
-        let p1 = createNode("h3");
+        let p1 = createNode("h5");
         p1.innerHTML = "Rating: " + movie.rating;
         append(movieInfo, p1);
         let p_blank = createNode("p")
         p_blank.innerHTML = ""
         append(movieInfo, p_blank)
-        let p2 = createNode("h3");
+        let p2 = createNode("h5");
         p2.innerHTML = "Description: ";
         append(movieInfo, p2);
         let p_des = createNode("p");
         p_des.innerHTML = movie.description;
         append(movieInfo, p_des);
-        let p3 = createNode("h3");
+        let p3 = createNode("h5");
         p3.innerHTML = "Storyline: ";
         append(movieInfo, p3);
         let p_sl = createNode("p");
         p_sl.innerHTML = movie.storyline;
         append(movieInfo, p_sl);
-        let p4 = createNode("h3");
+        let p4 = createNode("h5");
         let a = createNode("a");
         a.innerHTML = "IMDb Link";
         a.href = movie.imdbURL;
